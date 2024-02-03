f8533bf9b19127597dd1fedab1c9e28cc4db83fc
Enorezero <98647618+Enorezero@users.noreply.github.com>
2024-02-04 04:44:40 +1000

    defined and added main microservices

---
+++ notification-service/src/main/java/ru/enorezero/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,15 @@
+package ru.enorezero.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+}
---
+++ paste-service/src/test/java/ru/enorezero/pasteservice/PasteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.enorezero.pasteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
